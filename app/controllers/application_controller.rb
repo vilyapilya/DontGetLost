d7d81420f735669fb39946f95de20b66b023aba6
Tu Ngo <tungo8810@gmail.com>
2017-06-05 16:31:18 -0700

    Add Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,29 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?, :logged_out?
+
+  def current_user
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def logged_out?
+    current_user.nil?
+  end
+
+  def login(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def require_logged_in!
+    if !logged_in?
+      render json: ['Require authentication'], status: 422
+      return
+    end
+  end
 end
