9d5beb2dd185b95b3e19a8fae8e7eece0211989d
Vincent Chan <vincent.chan@biola.edu>
2017-06-05 17:00:27 -0700

    user and session models and controllers

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,30 @@
+class Api::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      sign_in(@user)
+      render "api/users/show"
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+
+  end
+
+  def update
+    @user = User.find_by(username: user_params[:username])
+
+    if @user
+      @user.update_attribute(image_url: user_params[:image_url])
+      render "api/users/show"
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+
+  end
+
+  def user_params
+
+    params.require(:user).permit(:username, :password, :image_url)
+  end
+end
