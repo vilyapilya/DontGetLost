4b2ea3449a28cfee67290541f2804ba190e72909
Vincent Chan <vincent.chan@biola.edu>
2017-06-07 10:03:03 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
       resources :users, only: [:create, :update, :index]
       resource :session, only: [:create, :destroy]
-
+      resources :groups, only: [:show, :index, :create, :destroy, :update]
+      resources :invitations, only: [:create, :destroy]
     end
 
 end
