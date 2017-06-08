74dda943b719216713b516ff62448ee2feff29bb
Vincent Chan <vincent.chan@biola.edu>
2017-06-08 08:56:03 -0700

    fetch requests

--- DontGetLost/App/App.js
+++ DontGetLost/App/App.js
@@ -4,8 +4,10 @@ import {Provider} from 'react-redux';
 import Root from './Root';
 
 import configureStore from './store/store';
-
+import {register} from './util/login_api_util';
 const store = configureStore();
+window.store = store;
+window.register = register;
 
 class App extends Component {
   render() {
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:create, :update]
+    resources :users, only: [:index, :create, :update]
     resource :session, only: [:create, :destroy]
     resources :groups, only: [:show, :index, :create, :destroy, :update]
     resources :invitations, only: [:create, :destroy]
