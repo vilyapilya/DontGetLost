eb9b3601ea6a8086ffb6f0f019e274abe6c90396
Tony Weng <tcweng91@gmail.com>
2017-06-09 10:32:42 -0700

    create group form, container

---
+++ DontGetLost/App/components/groups/group_form.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import { View, Text, ScrollView, StyleSheet, TouchableHighlight } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+
+export default class GroupDetail extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      group_name: ""
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit() {
+    const group = this.state;
+    this.props.createGroup(group);
+    
+  }
+
+  render() {
+    return (
+      <View style={{backgroundColor: 'blue', flex: 1}}>
+        <Text>Group Name</Text>
+        <TextInput onChangeText={(val) => this.setState({group_name: val})} placeholder="Group Name" />
+        <TouchableHighlight onPress={this.handleSubmit}>
+          <Text>Create Group</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
