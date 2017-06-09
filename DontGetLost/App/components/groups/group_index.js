c354023aa54b6d3d51efe2775594f94a8a5edf6e
Tony Weng <tcweng91@gmail.com>
2017-06-09 09:21:53 -0700

    start group index

--- DontGetLost/App/components/groups/group_index.js
+++ DontGetLost/App/components/groups/group_index.js
@@ -1,17 +1,27 @@
 import React, {Component} from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
 
 class GroupIndex extends Component{
+  constructor() {
+    super();
+  }
 
-
+  componentDidMount() {
+    this.props.requestAllPosts();
+  }
 
   render() {
-    return (
-      <View>
-
-      </View>
-    );
-
+    if (this.props.groups.length === 0) {
+      return null;
+    } else {
+      return (
+        <View>
+          <ScrollView>
+            
+          </ScrollView>
+        </View>
+      );
+    }
   }
 }
 
