4437ff9cc691d918fb356ea2525b7569adae762b
vilyapilya <poemsreader@gmail.com>
2017-06-11 00:08:48 -0700

    put marker on a map based on the device location

--- DontGetLost/App/components/Locat/locat.js
+++ DontGetLost/App/components/Locat/locat.js
@@ -1,20 +1,30 @@
 import React, {Component} from 'react';
 import { View, Text, TextInput, TouchableHighlight, AsyncStorage, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
 const ACCESS_TOKEN = 'acccess_token';
 
 class Locat extends Component{
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.onRegionChange = this.onRegionChange.bind(this);
+    this.getRandomInt = this.getRandomInt.bind(this);
     this.state = {
       latitude: null,
       longitude: null,
-      error: null,
+      latitudeDelta: 0.015,
+      longitudeDelta: 0.0121
     };
   }
+  getRandomInt(min, max){
+    var intMin = parseInt(min);
+    var intMax = parseInt(max);
+    return (Math.floor(Math.random() * (intMax-intMin)) + intMin);
+  }
+
   componentDidMount() {
-    navigator.geolocation.getCurrentPosition(
+    this.watchId = navigator.geolocation.watchPosition(
       (position) => {
+        console.log("watching");
         this.setState({
           latitude: position.coords.latitude,
           longitude: position.coords.longitude,
@@ -22,41 +32,68 @@ class Locat extends Component{
         });
       },
       (error) => this.setState({ error: error.message }),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 10 },
     );
   }
 
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchId);
+  }
+
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+  getInitialState() {
+    return {
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+    };
+  }
+
+
   render() {
-    console.log("map");
+    console.log("aaa");
     console.log(this.state.latitude);
+    let lat = this.state.latitude;
+    let lon = this.state.longitude;
     return (
       <View style ={styles.container}>
         <MapView
           style={styles.map}
           region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
+            latitude: lat,
+            longitude: lon,
             latitudeDelta: 0.015,
             longitudeDelta: 0.0121,
           }}
+          onRegionChange={this.onRegionChange}
         >
+        <MapView.Marker
+          coordinate={{
+            latitude: lat,
+            longitude: lon,
+          }} />
         </MapView>
       </View>
     );
-
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    flex :1,
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-});
+    container: {
+      ...StyleSheet.absoluteFillObject,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+     flex :1,
+    },
+    map: {
+      ...StyleSheet.absoluteFillObject,
+      flex :1,
+   },
+ });
 
 export default Locat;
