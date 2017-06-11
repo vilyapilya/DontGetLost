42e54d926c26f30070b496bcbfe6364028c9b46f
vilyapilya <poemsreader@gmail.com>
2017-06-10 19:52:11 -0700

    make map full screen

--- DontGetLost/App/components/Locat/locat.js
+++ DontGetLost/App/components/Locat/locat.js
@@ -50,10 +50,9 @@ class Locat extends Component{
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: 400,
     justifyContent: 'flex-end',
     alignItems: 'center',
+    flex :1,
   },
   map: {
     ...StyleSheet.absoluteFillObject,
